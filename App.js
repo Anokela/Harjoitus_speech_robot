8dc56a08f291ca92f548ae04400e979b79bb9818
Aleksi Nokela <n0noal00@students.oamk.fi>
2022-11-26 19:17:46 +0200

    push to repository

--- App.js
+++ App.js
@@ -1,11 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { View, StyleSheet, Button } from 'react-native';
+import * as Speech from 'expo-speech';
 
 export default function App() {
+  const aphorism = [
+    'Never', 'Trust', 'a', 'computer', 'you', 'can',
+    'not', 'throw', 'out', 'a', 'window',
+  ];
+  const NBR_OF_REPEATS = 5;
+
+  function  shuffle(array) {
+    let currentIndex = array.length, randomIndex;
+    while (currentIndex != 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+    return array;
+  }
+
+
+  const speak = () => {
+    for (let i = 0; i < NBR_OF_REPEATS; i++) {
+      let shuffledAphorism = shuffle(aphorism);
+      let thingToSay = '';
+      shuffledAphorism.map(word=> {
+        thingToSay += word + " ";
+      })
+      Speech.speak(thingToSay);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button title="Press to hear some words" onPress={speak} />
     </View>
   );
 }
